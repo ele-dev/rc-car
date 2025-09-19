e81b8d2640a0a012193771aab4c97786ec786d0a
Elias Geiger <elias@ele-dev.de>
2025-09-19 22:12:09 +0200

    added signal handler to exit via Ctrl+C

--- raspberrypi-onboard/src/main.cpp
+++ raspberrypi-onboard/src/main.cpp
@@ -7,17 +7,44 @@
 #include "SteeringController.h"
 
 #include <iostream>
+#include <signal.h>
 
+// function prototypes
+void terminateSignalHandler(int);
+
+// submodule instances
 MotorController motor;
 SteeringController steeringwheel;
 
-int main(void)
+// ------------ application main section ------------------- //
+
+int main(int argc, char **argv)
 {
     std::cout << "Launch RC Car controller application" << std::endl;
 
+    // create signal handler for clean Ctrl+C close up
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = terminateSignalHandler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
+    // infinite loop
+    while(true)
+    {
+        // ...
+    }
+
+    terminateSignalHandler(EXIT_SUCCESS);
+
+    return EXIT_SUCCESS;
+}
+
+void terminateSignalHandler(int code) {
+    // do cleanup tasks
     // ...
 
     std::cout << "Exit now." << std::endl;
 
-    return EXIT_SUCCESS;
+    exit(code);
 }
